39c0f9fb6b0cb1d9099561cb45f2dca4a7f099f7
nemo72999 <rohanjadhav1880@gmail.com>
2026-02-17 22:06:42 +0530

    Update service worker cache name to cosmic-v2.3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cosmic-v2.2";
+const CACHE_NAME = "cosmic-v2.3";
 
 const ASSETS = [
   "/",
@@ -47,6 +47,7 @@ self.addEventListener("fetch", (event) => {
     caches.match(req).then((cached) => cached || fetch(req))
   );
 });
+
 
 
 
