d00dec929846acb865f41b7af0ea93d9185ac2a5
nemo72999 <rohanjadhav1880@gmail.com>
2026-03-07 11:22:23 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cosmic-v2.8.1";
+const CACHE_NAME = "cosmic-v3";
 
 const ASSETS = [
   "/",
@@ -47,6 +47,7 @@ self.addEventListener("fetch", (event) => {
     caches.match(req).then((cached) => cached || fetch(req))
   );
 });
+
 
 
 
